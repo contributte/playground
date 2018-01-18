d0db089dd24c8120c87517aa7eef983cacfbb02a
Milan Felix Šulc <rkfelix@gmail.com>
2018-01-18 11:39:07 +0100

    [Micro] Upgrade to v0.2-dev

--- micro/app/controllers/HelloController.php
+++ micro/app/controllers/HelloController.php
@@ -6,6 +6,8 @@ use Apitte\Core\Annotation\Controller\Controller;
 use Apitte\Core\Annotation\Controller\ControllerPath;
 use Apitte\Core\Annotation\Controller\Method;
 use Apitte\Core\Annotation\Controller\Path;
+use Apitte\Core\Annotation\Controller\RequestParameter;
+use Apitte\Core\Annotation\Controller\RequestParameters;
 use Apitte\Core\Http\ApiRequest;
 use Apitte\Core\Http\ApiResponse;
 use Apitte\Core\UI\Controller\AbstractController;
@@ -24,6 +26,18 @@ final class HelloController extends AbstractController
 	public function index(ApiRequest $request, ApiResponse $response)
 	{
 		return $response->writeJsonBody(['hello' => ['world']]);
+	}
+
+	/**
+	 * @Path("/filter")
+	 * @Method("GET")
+	 * @RequestParameters({
+	 *     @RequestParameter(name="id", type="int", in="query")
+	 * })
+	 */
+	public function filter(ApiRequest $request, ApiResponse $response)
+	{
+		return $response->writeJsonBody(['filter' => [$request->getParameters()]]);
 	}
 
 }
