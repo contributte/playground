bff4934ea0ecd9876c5bf4f35e6299bcdedbb492
Marek Bartoš <bartos.developer152@gmail.com>
2018-07-29 15:36:20 +0200

    Live form validation (#15)

---
+++ live-form-validation/app/bootstrap.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+use Contributte\Bootstrap\ExtraConfigurator;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new ExtraConfigurator();
+
+$configurator->setDebugMode(true);
+$configurator->enableTracy(__DIR__ . '/../log');
+
+$configurator->setTimeZone('Europe/Prague');
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+
+return $configurator->createContainer();
