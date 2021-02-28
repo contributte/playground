99e2aab11b68505785683a367abe67db0e3e0025
Petr Parolek <petr.parolek@gmail.com>
2021-02-28 18:26:17 +0100

    added example of using contributte/pdf

---
+++ contributte-pdf/app/Bootstrap.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace App;
+
+use Nette\Configurator;
+
+class Bootstrap
+{
+
+	public static function boot(): Configurator
+	{
+		$configurator = new Configurator();
+
+		$configurator->setDebugMode(true);
+		$configurator->enableTracy(__DIR__ . '/../log');
+
+		$configurator->setTimeZone('Europe/Prague');
+		$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+		$configurator->createRobotLoader()
+			->addDirectory(__DIR__)
+			->register();
+
+		$configurator->addConfig(__DIR__ . '/config/common.neon');
+
+		return $configurator;
+	}
+
+}
